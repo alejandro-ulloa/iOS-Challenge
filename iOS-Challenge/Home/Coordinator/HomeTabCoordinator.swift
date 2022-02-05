28cb09e3dd2f2f0ccaa50c835091a48df27f738d
Alejandro Ulloa <alejandro.ulloa@devsu.com>
2022-02-05 13:13:23 -0500

    created tab coordinator

---
+++ iOS-Challenge/Home/Coordinator/HomeTabCoordinator.swift
@@ -0,0 +1,41 @@
+//
+//  HomeTabCoordinator.swift
+//  iOS-Challenge
+//
+//  Created by Alejandro Ulloa on 2022-02-05.
+//
+
+import Foundation
+import XCoordinator
+import RxSwift
+
+enum HomeTabRoute: Route {
+    case shows
+    case favorites
+}
+
+final class HomeTabCoordinator: TabBarCoordinator<HomeRoute> {
+    
+    
+    init() {
+        
+        let homeCoordinator = HomeCoordinator()
+        homeCoordinator.rootViewController.tabBarItem = UITabBarItem(title: "shows_tab".localized, image: UIImage(systemName: "list.bullet"), tag: 0)
+        
+        let favoritesCoordinator = HomeCoordinator()
+        favoritesCoordinator.rootViewController.tabBarItem = UITabBarItem(title: "favorites_tab".localized, image: UIImage(systemName: "star"), tag: 0)
+        
+        super.init(rootViewController: MainViewController(), tabs: [homeCoordinator.strongRouter, favoritesCoordinator.strongRouter],
+                   select: homeCoordinator)
+    }
+}
+
+
+class MainViewController: UITabBarController, UITabBarControllerDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        delegate = self
+    }
+    
+}

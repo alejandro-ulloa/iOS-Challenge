75d8d0ccc68a01955838ad8fd34d982dca542438
Alejandro Ulloa <alejandro.ulloa@devsu.com>
2022-02-06 23:08:30 -0500

    genre added to description

--- iOS-Challenge/Home/View/ShowDetailViewController.swift
+++ iOS-Challenge/Home/View/ShowDetailViewController.swift
@@ -20,6 +20,8 @@ final class ShowDetailViewController: BaseViewController, BindableType {
     let topStackView = UIStackView()
     let showImageView = UIImageView()
     let detailStackView = UIStackView()
+    let genresTitleLabel = UILabel()
+    let genresLabel = UILabel()
     
     let summaryLabel = UILabel()
     
@@ -39,7 +41,7 @@ final class ShowDetailViewController: BaseViewController, BindableType {
         
         topStackView.axis = .horizontal
         topStackView.distribution = .fillEqually
-        topStackView.alignment = .center
+        topStackView.alignment = .top
         topStackView.spacing = 10
         mainStackView.addArrangedSubview(topStackView)
         
@@ -48,9 +50,16 @@ final class ShowDetailViewController: BaseViewController, BindableType {
         
         detailStackView.axis = .vertical
         detailStackView.distribution = .fillProportionally
-        detailStackView.alignment = .center
-        detailStackView.backgroundColor = .red
+        detailStackView.alignment = .leading
+        detailStackView.spacing = 10
         topStackView.addArrangedSubview(detailStackView)
+        
+        genresTitleLabel.font = .systemFont(ofSize: 14, weight: .bold)
+        detailStackView.addArrangedSubview(genresTitleLabel)
+        
+        genresLabel.numberOfLines = 0
+        genresLabel.font = .systemFont(ofSize: 12)
+        detailStackView.addArrangedSubview(genresLabel)
         
         summaryLabel.numberOfLines = 0
         summaryLabel.textAlignment = .justified
@@ -64,6 +73,8 @@ final class ShowDetailViewController: BaseViewController, BindableType {
         if let imageURLString = viewModel.show.image?.original, let imageURL = URL(string: imageURLString) {
             showImageView.kf.setImage(with: imageURL)
         }
+        genresTitleLabel.text = "genre_label".localized
+        genresLabel.text = viewModel.show.genres?.joined(separator: ", ")
         summaryLabel.text = viewModel.show.summary?.htmlTransform()
     }
     
@@ -73,20 +84,16 @@ final class ShowDetailViewController: BaseViewController, BindableType {
         }
         
         mainStackView.snp.makeConstraints {
-            $0.edges.equalTo(view)
+            $0.edges.equalTo(view).inset(UIEdgeInsets(top: 20, left: 15, bottom: 20, right: 15))
         }
         
         topStackView.snp.makeConstraints {
-            $0.width.equalTo(mainStackView).multipliedBy(0.9)
-            $0.height.equalTo(view).multipliedBy(0.4)
-        }
-        
-        detailStackView.snp.makeConstraints {
-            $0.height.equalToSuperview()
+            $0.width.equalTo(mainStackView)
+            $0.height.equalTo(view).multipliedBy(0.35)
         }
         
         summaryLabel.snp.makeConstraints {
-            $0.width.equalToSuperview().multipliedBy(0.9)
+            $0.width.equalToSuperview()
         }
     }
     
